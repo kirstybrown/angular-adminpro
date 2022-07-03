ab68025d4a3aedc92f04e3368d0b7d82907eb79f
Kirsty Brown <kirsty.a.d.brown@gmail.com>
2022-07-03 21:51:34 +0200

    Added pages component for secondary routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,34 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { Graphics1Component } from './pages/graphics1/graphics1.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { PagesComponent } from './pages/pages/pages.component';
+import { ProgressComponent } from './pages/progress/progress.component';
 
+const routes: Routes = [
+  {
+    path: '', component: PagesComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent},
+      { path: 'progress', component: ProgressComponent },
+      { path: 'graphics1', component: Graphics1Component },
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    ]
+  },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+
+  { path: '**', component: NopagefoundComponent },
+]
 
 @NgModule({
   declarations: [],
-  imports: []
+  imports: [
+    RouterModule.forRoot( routes )
+  ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
@@ -11,6 +13,7 @@ import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { ProgressComponent } from './pages/progress/progress.component';
 import { Graphics1Component } from './pages/graphics1/graphics1.component';
+import { PagesComponent } from './pages/pages/pages.component';
 
 @NgModule({
   declarations: [
@@ -23,12 +26,13 @@ import { Graphics1Component } from './pages/graphics1/graphics1.component';
     SidebarComponent,
     HeaderComponent,
     ProgressComponent,
-    Graphics1Component
+    Graphics1Component,
+    PagesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
